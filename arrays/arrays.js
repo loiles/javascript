e198bfcf1cab69aad4c5bdbf6914caa2099b3ed1
loiles <luisasantos084@gmail.com>
2025-07-03 17:49:28 -0300

    commit

---
+++ arrays/arrays.js
@@ -0,0 +1,26 @@
+
+//const convidado1= "Matheus";
+//const convidado2= "Amanda";
+//console.log("Lista de convidados:", convidado1, convidado2);
+
+const listadeconvidados = ["Matheus", "Izabelli", "Amanda", "Paula"]; //length 4
+
+console.log("Luísa fez aniversário e convidou:", listadeconvidados);
+console.log("quantos convidados?", listadeconvidados.length);
+// 1- Matheus
+//2- Izabelli
+//3- Amanda
+//4- Paula
+
+console.log("convidados numero 1:", listadeconvidados[0]);
+//console.log("convidados numero 4:", listadeconvidados[3]);
+console.log("convidados numero 4:", listadeconvidados[listadeconvidados.length -1]);
+console.log("convidado numero 3:", listadeconvidados [2]);
+
+const indiceAmanda =  listadeconvidados.indexOf("Amanda");
+console.log("Amanda esta em qual indice do nosso array", indiceAmanda);
+console.log("Amanda esta em qual numero da nossa lista", indiceAmanda +1);
+
+
+
+
