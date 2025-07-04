4b47e36f15fc6d11b5fb2502f92d287949426f2c
loiles <luisasantos084@gmail.com>
2025-07-04 18:02:56 -0300

    commit

---
+++ arrays/funcoes/funçoes.js
@@ -0,0 +1,13 @@
+
+function imprimir(){
+    console.log("Olá, Mundo!");
+}
+
+imprimir();
+
+function calcularSoma (valor1, valor2){
+    return valor1 + valor2;
+}
+const valor1 = 2;
+const valor2 = 4;
+console.log(`A soma entre ${valor1} e ${valor2} é ${calcularSoma(valor1, valor2)}`);
